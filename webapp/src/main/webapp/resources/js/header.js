a01803adf1f4296f0581d364de83b2db251e4fc9
pvedoya <pvedoya@itba.edu.ar>
2020-04-13 12:21:13 -0300

    search param now stays in searchbar

--- webapp/src/main/webapp/resources/js/header.js
+++ webapp/src/main/webapp/resources/js/header.js
@@ -1,9 +1,19 @@
 $('#search-button').on('click',function(evt){
     let string = '?';
+    const value = $('#search-value').val();
 
-    string+="search=" + $('#search-value').val() + '&';
+    string+="search=" + value + '&';
     const query = string;
 
     location.href = window.location.href.split('?')[0] + query;
+});
 
+$(document).ready(function(event){
+    if(!window.location.href.includes('?'))
+        return;
+
+    const query = window.location.href.split('=')[1];
+    const search = query.split('&')[0];
+
+    $('#search-value').val(search);
 });
