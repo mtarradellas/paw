e56bd97d92f18cdbfcd0a23a32a3cc3206626f0b
mtarradellas <mtarradellas@itba.edu.ar>
2020-04-26 16:10:26 -0300

    new parent controller and logged user method

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HomeController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HomeController.java
@@ -1,27 +1,11 @@
 package ar.edu.itba.paw.webapp.controller;
 
-import ar.edu.itba.paw.interfaces.ImageService;
-import ar.edu.itba.paw.interfaces.PetService;
-import ar.edu.itba.paw.interfaces.SpeciesService;
-import ar.edu.itba.paw.interfaces.UserService;
-import ar.edu.itba.paw.webapp.exception.PetNotFoundException;
-import ar.edu.itba.paw.webapp.exception.UserNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Locale;
-
 @Controller
-public class HomeController {
-
-    @Autowired
-    SpeciesService speciesService;
-    @Autowired
-    PetService petService;
+public class HomeController extends ParentController {
 
     @RequestMapping("/available")
     public ModelAndView getAvailable() {
@@ -36,10 +20,6 @@ public class HomeController {
     @RequestMapping(value = "/test")
     public ModelAndView getIdPet() {
         final ModelAndView mav = new ModelAndView("views/test");
-
-        mav.addObject("pet_list",
-                petService.list("en_US","all"));
         return mav;
     }
-
 }
