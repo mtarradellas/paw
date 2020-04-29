08ab254b3c8e111732251218bef911ecd38d4313
pvedoya <pvedoya@itba.edu.ar>
2020-04-29 14:58:46 -0300

    More formail mails, they now have page url

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/LoginAndRegisterController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/LoginAndRegisterController.java
@@ -1,5 +1,4 @@
 package ar.edu.itba.paw.webapp.controller;
-
 import ar.edu.itba.paw.interfaces.MailService;
 import ar.edu.itba.paw.interfaces.exception.DuplicateUserException;
 import ar.edu.itba.paw.models.Token;
@@ -20,43 +19,35 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Optional;
 import java.util.UUID;
-
 @Controller
 @ComponentScan("ar.edu.itba.paw.webapp.auth")
 public class LoginAndRegisterController extends ParentController {
-
     @Autowired
     private PSUserDetailsService userDetailsService;
-
     @Autowired
     private MailService mailService;
-
     @RequestMapping("/login")
     public ModelAndView login() {
         return new ModelAndView("views/login");
     }
-
     @RequestMapping(value ="/register", method = { RequestMethod.GET })
     public ModelAndView registerForm(@ModelAttribute ("registerForm") final UserForm userForm) {
         return new ModelAndView("views/register");
     }
-
     @RequestMapping(value = "/register", method = { RequestMethod.POST })
     public ModelAndView createUser(@Valid @ModelAttribute("registerForm") final UserForm userForm,
                                    final BindingResult errors, HttpServletRequest request) {
-
         if (errors.hasErrors()) {
             return registerForm(userForm);
         }
-
         Optional<User> opUser;
         try {
             opUser = userService.create(userForm.getUsername(), userForm.getPassword(),
@@ -66,21 +57,33 @@ public class LoginAndRegisterController extends ParentController {
                     .addObject("duplicatedUsername", ex.isDuplicatedUsername())
                     .addObject("duplicatedMail", ex.isDuplicatedMail());
         }
-
         if (opUser == null || !opUser.isPresent()) {
             return registerForm(userForm).addObject("generalError", true);
         }
-
-        authenticateUserAndSetSession(opUser.get().getUsername(), request);
-
-        return new ModelAndView("redirect:/");
+        // authenticateUserAndSetSession(opUser.get().getUsername(), request);
+        UUID uuid = UUID.randomUUID();
+        userService.createToken(uuid, opUser.get().getId());
+        mailService.sendMail(opUser.get().getMail(),activateAccountSubject(),activateAccountBody(opUser.get(),uuid));
+        return new ModelAndView("views/email_sent_for_password_reset");
     }
-
+    @RequestMapping(value ="/account-activation", method = { RequestMethod.GET })
+    public ModelAndView requestResetPassword(@RequestParam (name = "token", required = true) String tokenString) {
+        UUID uuid = UUID.fromString(tokenString);
+        Optional<Token> token = userService.getToken(uuid);
+        if(!token.isPresent() || new Date().after(token.get().getExpirationDate())){
+            return new ModelAndView("views/token_has_expired");
+        }
+        Optional<User> opUser = userService.findByToken(uuid);
+        if(!opUser.isPresent()){
+            return new ModelAndView("error-views/404");
+        }
+        userService.deleteToken(uuid);
+        return new ModelAndView("redirect:/login");
+    }
     @RequestMapping(value ="/request-password-reset", method = { RequestMethod.GET })
     public ModelAndView requestResetPassword(@ModelAttribute ("mailForm") final RequestMail mailForm) {
         return new ModelAndView("views/request_password_reset");
     }
-
     @RequestMapping(value ="/request-password-reset", method = { RequestMethod.POST })
     public ModelAndView requestResetPassword(@Valid @ModelAttribute ("mailForm") final RequestMail mailForm, final BindingResult errors) {
         if (errors.hasErrors()) {
@@ -88,31 +91,25 @@ public class LoginAndRegisterController extends ParentController {
         }
         Optional<User> opUser = userService.findByMail(mailForm.getMail());
         if(opUser.isPresent()){
-
             UUID uuid = UUID.randomUUID();
             userService.createToken(uuid, opUser.get().getId());
             mailService.sendMail(opUser.get().getMail(),resetPasswordSubject(),resetPasswordBody(opUser.get(),uuid));
             return new ModelAndView("views/email_sent_for_password_reset");
-
         }
         return requestResetPassword(mailForm)
                 .addObject("invalid_mail", true);
     }
-
     @RequestMapping(value ="/password-reset", method = { RequestMethod.GET })
     public ModelAndView resetPassword(@ModelAttribute ("resetPasswordForm") final ResetPasswordForm resetPasswordForm) {
         return new ModelAndView("views/password_reset");
     }
-
     @RequestMapping(value ="/password-reset", method = { RequestMethod.POST })
     public ModelAndView resetPassword(@Valid @ModelAttribute ("resetPasswordForm") final ResetPasswordForm resetPasswordForm, final BindingResult errors) {
         if (errors.hasErrors()) {
             return resetPassword(resetPasswordForm);
         }
-
         UUID uuid = UUID.fromString(resetPasswordForm.getToken());
         Optional<Token> token = userService.getToken(uuid);
-
         if(!token.isPresent() || new Date().after(token.get().getExpirationDate())){
             return new ModelAndView("views/token_has_expired");
         }
@@ -124,38 +121,27 @@ public class LoginAndRegisterController extends ParentController {
         userService.deleteToken(uuid);
         return new ModelAndView("redirect:/login");
     }
-
     @RequestMapping(value ="/request-link-account", method = { RequestMethod.GET })
     public ModelAndView requestLinkAccount(@ModelAttribute ("mailForm") final RequestMail mailForm) {
         return new ModelAndView("views/request_password_reset");
     }
-
     @RequestMapping(value ="/request-link-account", method = { RequestMethod.POST })
     public ModelAndView requestLinkAccount(@Valid @ModelAttribute ("mailForm") final RequestMail mailForm, final BindingResult errors) {
         return requestResetPassword(mailForm,errors);
     }
-
-
     public Authentication authenticateUserAndSetSession(String username,HttpServletRequest request){
-
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-
         Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
-
         SecurityContextHolder.getContext().setAuthentication(authentication);
-
         request.getSession().setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
-
         return authentication;
     }
-
     @RequestMapping(value = "/403")
     public ModelAndView accessDenied() {
         return new ModelAndView("error-views/403");
     }
-
     private String resetPasswordBody(User user, UUID uuid) {
-        String url = "http://pawserver.it.itba.edu.ar/paw-2020a-7/password-reset";
+        String url = "password-reset";
         url += "?token=" + uuid;
         String body;
         if(getLocale().equals("en_US")) {
@@ -179,5 +165,31 @@ public class LoginAndRegisterController extends ParentController {
         }
         else { subject = "Resetea tu contraseña"; }
         return subject;
+    }
+    private String activateAccountSubject() {
+        String subject;
+        if(getLocale().equals("en_US")) {
+            subject = "Activate your account";
+        }
+        else { subject = "Activa tu cuenta"; }
+        return subject;
+    }
+    private String activateAccountBody(User user, UUID uuid) {
+        String url = "http://pawserver.it.itba.edu.ar/paw-2020a-7/account-activation";
+        url += "?token=" + uuid;
+        String body;
+        if(getLocale().equals("en_US")) {
+            body = "Hello " + user.getUsername() +
+                    ",\nPlease click the link below to activate your account\n"
+                    + url +
+                    "\nSincerely,\nPet Society Team.";
+        }
+        else{
+            body = "Hola " + user.getUsername() +
+                    ",\nPor favor haz click en el siguiente link para activar tu cuenta\n"
+                    + url +
+                    "\nSinceramente,\nEl equipo de Pet Society.";
+        }
+        return body;
     }
 }
