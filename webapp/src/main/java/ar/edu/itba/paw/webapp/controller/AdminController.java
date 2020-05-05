7cf150eb48162856569a1708096d9330e5991ea1
pvedoya <pvedoya@itba.edu.ar>
2020-05-05 15:16:23 -0300

    redirect from main page

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/AdminController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/AdminController.java
@@ -1,18 +1,25 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class AdminController extends ParentController{
 
-    @RequestMapping(value = "/admin/{id}")
-    public ModelAndView getAvailable(@PathVariable("id") long id) {
+//    @RequestMapping(value = "/admin/{id}")
+//    public ModelAndView getAvailable(@PathVariable("id") long id) {
+//
+//        return new ModelAndView("views/admin");
+//    }
+
+    @RequestMapping(value = "/adm")
+    public ModelAndView getAvailable() {
 
         return new ModelAndView("views/admin");
     }
-
 
 
 }
