dbbbc8be08e356d7eec6693064708fb15769f045
mtarradellas <mtarradellas@itba.edu.ar>
2020-05-07 12:45:26 -0300

    pet controller logging

--- interfaces/src/main/java/ar/edu/itba/paw/interfaces/PetService.java
+++ interfaces/src/main/java/ar/edu/itba/paw/interfaces/PetService.java
@@ -14,7 +14,8 @@ public interface PetService {
     List<Pet> filteredList(String language, String specie, String  breed, String gender, String searchCriteria, String searchOrder, String page);
     List<Pet> getByUserId(String language, long userId, String page);
     Pet create(String language, String petName, String speciesName, String breedName, String location, boolean vaccinated, String gender, String description, Date birthDate, Date uploadDate, int price, long ownerId);
-    boolean updateStatus(long petId, long userId, long newStatus);
+    boolean sellPet(long petId, long userId);
+    boolean removePet(long petId, long userId);
     String getMaxPages();
     String getMaxSearchPages(String language, String findValue);
     String getMaxFilterPages(String language, String specieFilter, String breedFilter, String genderFilter);
