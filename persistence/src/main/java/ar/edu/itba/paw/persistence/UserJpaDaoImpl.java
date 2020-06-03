6118aa2fc0056fc2e087c31bed9366583ee7b7f7
lkarpovich <lkarpovich@hotmail.com>
2020-06-03 12:52:10 -0300

    patch to userdao

--- persistence/src/main/java/ar/edu/itba/paw/persistence/UserJpaDaoImpl.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/UserJpaDaoImpl.java
@@ -45,7 +45,8 @@ public class UserJpaDaoImpl implements UserDao {
     @Override
     public int getListAmount() {
         Query nativeQuery = em.createNativeQuery("SELECT count(*) FROM users");
-        return (int) nativeQuery.getSingleResult();
+        Number count = (Number) nativeQuery.getSingleResult();
+        return count.intValue();
     }
 
     @Override
