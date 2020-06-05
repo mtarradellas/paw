eefe81f820d46e84cd2953e847f2fbb751948467
mtarradellas <mtarradellas@itba.edu.ar>
2020-06-05 20:17:15 -0300

    tests updated but EM not working

--- models/src/main/java/ar/edu/itba/paw/models/Pet.java
+++ models/src/main/java/ar/edu/itba/paw/models/Pet.java
@@ -184,6 +184,10 @@ public class Pet {
         return images;
     }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public void setPetName(String petName) {
         this.petName = petName;
     }
