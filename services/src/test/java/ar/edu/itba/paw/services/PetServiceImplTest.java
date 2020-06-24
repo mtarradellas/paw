b20c981b0c6b33b8e1afb9b38ed7919489774b87
mtarradellas <mtarradellas@itba.edu.ar>
2020-06-24 13:20:22 -0300

    petService test almost complete WIP

--- services/src/test/java/ar/edu/itba/paw/services/PetServiceImplTest.java
+++ services/src/test/java/ar/edu/itba/paw/services/PetServiceImplTest.java
@@ -3,6 +3,7 @@ package ar.edu.itba.paw.services;
 import ar.edu.itba.paw.interfaces.*;
 import ar.edu.itba.paw.models.*;
 import ar.edu.itba.paw.models.constants.PetStatus;
+import ar.edu.itba.paw.models.constants.QuestionStatus;
 import ar.edu.itba.paw.models.constants.UserStatus;
 import org.junit.Assert;
 import org.junit.Before;
@@ -17,6 +18,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -83,6 +85,11 @@ public class PetServiceImplTest {
     private Pet O_PET = new Pet("other_pet", LocalDateTime.now().minusMonths(3L), "another", false, 500, LocalDateTime.now().minusMonths(3L),
             "other pet", PetStatus.UNAVAILABLE, O_USER, O_SPECIES, O_BREED, O_PROVINCE, O_DEPARTMENT);
 
+    /* COMMENTS */
+    private static final Long QUESTION_ID = 1L;
+    private Question QUESTION = new Question("hello?", USER, O_USER, PET, LocalDateTime.now(), QuestionStatus.VALID);
+    private static final Long ANSWER_ID = 1L;
+    private Answer ANSWER = new Answer(QUESTION, "hi", O_USER, USER, PET, LocalDateTime.now(), QuestionStatus.VALID);
 
     private static final int PAGE = 1;
     private static final int PAGE_SIZE = 50;
@@ -140,14 +147,99 @@ public class PetServiceImplTest {
     }
 
     @Test
+    public void testFilteredListSpecies() {
+        List<Pet> petList = new ArrayList<>();
+        petList.add(PET);
+        when(petDao.searchList(any(), any(), any(), eq(PET.getSpecies()), any(), any(), any(), any(), any(), anyInt(), anyInt(), any(),
+                any(), anyInt(), anyInt())).thenReturn(petList);
+        when(speciesService.findSpeciesById(eq(PET.getSpecies().getId()))).thenReturn(Optional.of(PET.getSpecies()));
+
+        List<Pet> returnList = petServiceImpl.filteredList(LOCALE, null, null, PET.getSpecies().getId(), null, null,
+                null, null, null, 0, -1, null, null, PAGE, PAGE_SIZE);
+
+        assertEquals(1, returnList.size());
+        assertPet(returnList.get(0), PET.getId(), PET.getPetName(), PET.getBirthDate(), PET.getGender(), PET.isVaccinated(),
+                PET.getPrice(), PET.getUploadDate(), PET.getDescription(), PET.getStatus(), PET.getUser(),
+                PET.getSpecies(), PET.getBreed(), PET.getProvince(), PET.getDepartment());
+    }
+
+    @Test
+    public void testFilteredListBreed() {
+        List<Pet> petList = new ArrayList<>();
+        petList.add(PET);
+        when(petDao.searchList(any(), any(), any(), eq(PET.getSpecies()), eq(PET.getBreed()), any(), any(), any(), any(), anyInt(), anyInt(), any(),
+                any(), anyInt(), anyInt())).thenReturn(petList);
+        when(speciesService.findSpeciesById(eq(PET.getSpecies().getId()))).thenReturn(Optional.of(PET.getSpecies()));
+        when(speciesService.findBreedById(eq(PET.getBreed().getId()))).thenReturn(Optional.of(PET.getBreed()));
+
+        List<Pet> returnList = petServiceImpl.filteredList(LOCALE, null, null, PET.getSpecies().getId(), PET.getBreed().getId(), null,
+                null, null, null, 0, -1, null, null, PAGE, PAGE_SIZE);
+
+        assertEquals(1, returnList.size());
+        assertPet(returnList.get(0), PET.getId(), PET.getPetName(), PET.getBirthDate(), PET.getGender(), PET.isVaccinated(),
+                PET.getPrice(), PET.getUploadDate(), PET.getDescription(), PET.getStatus(), PET.getUser(),
+                PET.getSpecies(), PET.getBreed(), PET.getProvince(), PET.getDepartment());
+    }
+
+    @Test
+    public void testFilteredListProvince() {
+        List<Pet> petList = new ArrayList<>();
+        petList.add(PET);
+        when(petDao.searchList(any(), any(), any(), any(), any(), any(), any(), any(), any(), anyInt(), anyInt(), eq(PET.getProvince()),
+                any(), anyInt(), anyInt())).thenReturn(petList);
+        when(locationService.findProvinceById(eq(PET.getProvince().getId()))).thenReturn(Optional.of(PET.getProvince()));
+
+        List<Pet> returnList = petServiceImpl.filteredList(LOCALE, null, null, null, null, null,
+                null, null, null, 0, -1, PET.getProvince().getId(), null, PAGE, PAGE_SIZE);
+
+        assertEquals(1, returnList.size());
+        assertPet(returnList.get(0), PET.getId(), PET.getPetName(), PET.getBirthDate(), PET.getGender(), PET.isVaccinated(),
+                PET.getPrice(), PET.getUploadDate(), PET.getDescription(), PET.getStatus(), PET.getUser(),
+                PET.getSpecies(), PET.getBreed(), PET.getProvince(), PET.getDepartment());
+    }
+
+    @Test
+    public void testFilteredListDepartment() {
+        List<Pet> petList = new ArrayList<>();
+        petList.add(PET);
+        when(petDao.searchList(any(), any(), any(), any(), any(), any(), any(), any(), any(), anyInt(), anyInt(), eq(PET.getProvince()),
+                eq(PET.getDepartment()), anyInt(), anyInt())).thenReturn(petList);
+        when(locationService.findProvinceById(eq(PET.getProvince().getId()))).thenReturn(Optional.of(PET.getProvince()));
+        when(locationService.findDepartmentById(eq(PET.getDepartment().getId()))).thenReturn(Optional.of(PET.getDepartment()));
+
+        List<Pet> returnList = petServiceImpl.filteredList(LOCALE, null, null, null, null, null,
+                null, null, null, 0, -1, PET.getProvince().getId(), PET.getDepartment().getId(), PAGE, PAGE_SIZE);
+
+        assertEquals(1, returnList.size());
+        assertPet(returnList.get(0), PET.getId(), PET.getPetName(), PET.getBirthDate(), PET.getGender(), PET.isVaccinated(),
+                PET.getPrice(), PET.getUploadDate(), PET.getDescription(), PET.getStatus(), PET.getUser(),
+                PET.getSpecies(), PET.getBreed(), PET.getProvince(), PET.getDepartment());
+    }
+
+    @Test
+    public void testListByUser() {
+        List<Pet> petList = new ArrayList<>();
+        petList.add(PET);
+        when(petDao.listByUser(eq(PET.getUser().getId()), anyInt(), anyInt())).thenReturn(petList);
+
+        List<Pet> returnList = petServiceImpl.listByUser(LOCALE, PET.getUser().getId(), PAGE, PAGE_SIZE);
+
+        assertEquals(1, returnList.size());
+        assertPet(returnList.get(0), PET.getId(), PET.getPetName(), PET.getBirthDate(), PET.getGender(), PET.isVaccinated(),
+                PET.getPrice(), PET.getUploadDate(), PET.getDescription(), PET.getStatus(), PET.getUser(),
+                PET.getSpecies(), PET.getBreed(), PET.getProvince(), PET.getDepartment());
+    }
+
+    @Test
     public void testCreatePet() {
-        when(petDao.create(anyString(), any(), anyString(), anyBoolean(), anyInt(), any(), anyString(), any(), any(),
-                any(), any(), any(), any())).thenReturn(PET);
-        when(locationService.findProvinceById(anyLong())).thenReturn(Optional.of(PROVINCE));
-        when(locationService.findDepartmentById(anyLong())).thenReturn(Optional.of(DEPARTMENT));
-        when(speciesService.findSpeciesById(anyString(), anyLong())).thenReturn(Optional.of(SPECIES));
-        when(speciesService.findBreedById(anyString(), anyLong())).thenReturn(Optional.of(BREED));
-        when(userService.findById(anyLong())).thenReturn(Optional.of(USER));
+        when(petDao.create(eq(PET.getPetName()), eq(PET.getBirthDate()), eq(PET.getGender()), eq(PET.isVaccinated()),
+                eq(PET.getPrice()), eq(PET.getUploadDate()), eq(PET.getDescription()), eq(PET.getStatus()), eq(PET.getUser()), eq(PET.getSpecies()),
+                eq(PET.getBreed()), eq(PET.getProvince()), eq(PET.getDepartment()))).thenReturn(PET);
+        when(locationService.findProvinceById(anyLong())).thenReturn(Optional.of(PET.getProvince()));
+        when(locationService.findDepartmentById(anyLong())).thenReturn(Optional.of(PET.getDepartment()));
+        when(speciesService.findSpeciesById(anyString(), anyLong())).thenReturn(Optional.of(PET.getSpecies()));
+        when(speciesService.findBreedById(anyString(), anyLong())).thenReturn(Optional.of(PET.getBreed()));
+        when(userService.findById(anyLong())).thenReturn(Optional.of(PET.getUser()));
 
         Optional<Pet> opPet = Optional.empty();
         try {
@@ -164,6 +256,50 @@ public class PetServiceImplTest {
         assertPet(opPet.get(), PET.getId(), PET.getPetName(), PET.getBirthDate(), PET.getGender(), PET.isVaccinated(),
                 PET.getPrice(), PET.getUploadDate(), PET.getDescription(), PET.getStatus(), PET.getUser(),
                 PET.getSpecies(), PET.getBreed(), PET.getProvince(), PET.getDepartment());
-
     }
+
+    @Test
+    public void testListQuestions() {
+        List<Question> questionList = new ArrayList<>();
+        questionList.add(QUESTION);
+        when(petDao.listQuestions(eq(QUESTION.getPet().getId()), anyInt(), anyInt())).thenReturn(questionList);
+
+        List<Question> returnList = petServiceImpl.listQuestions(QUESTION.getPet().getId(), PAGE, PAGE_SIZE);
+
+        assertEquals(1, returnList.size());
+        Question question = returnList.get(0);
+        assertEquals(QUESTION.getId(), question.getId());
+        Pet pet = QUESTION.getPet();
+        assertPet(question.getPet(), pet.getId(), pet.getPetName(), pet.getBirthDate(), pet.getGender(), pet.isVaccinated(),
+                pet.getPrice(), pet.getUploadDate(), pet.getDescription(), pet.getStatus(), pet.getUser(),
+                pet.getSpecies(), pet.getBreed(), pet.getProvince(), pet.getDepartment());
+    }
+
+    @Test
+    public void testCreateQuestion() {
+        when(petDao.createQuestion(eq(QUESTION.getContent()), eq(QUESTION.getUser()), eq(QUESTION.getTarget()),
+                eq(QUESTION.getPet()), eq(QUESTION.getStatus()))).thenReturn(QUESTION);
+        when(petDao.findById(eq(QUESTION.getPet().getId()))).thenReturn(Optional.of(PET));
+        when(userService.findById(eq(QUESTION.getUser().getId()))).thenReturn(Optional.of(QUESTION.getUser()));
+        when(userService.findById(eq(QUESTION.getTarget().getId()))).thenReturn(Optional.of(QUESTION.getTarget()));
+
+        Optional<Question> opQuestion = Optional.empty();
+        try {
+            opQuestion = petServiceImpl.createQuestion(QUESTION.getContent(), QUESTION.getUser(), QUESTION.getPet().getId(), null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail("unexpected error during operation create question");
+        }
+
+        assertTrue(opQuestion.isPresent());
+        Question question = opQuestion.get();
+        assertEquals(question.getId(), QUESTION.getId());
+        assertEquals(question.getContent(), QUESTION.getContent());
+        Pet pet = QUESTION.getPet();
+        assertPet(question.getPet(), pet.getId(), pet.getPetName(), pet.getBirthDate(), pet.getGender(), pet.isVaccinated(),
+                pet.getPrice(), pet.getUploadDate(), pet.getDescription(), pet.getStatus(), pet.getUser(),
+                pet.getSpecies(), pet.getBreed(), pet.getProvince(), pet.getDepartment());
+    }
+
+
 }
