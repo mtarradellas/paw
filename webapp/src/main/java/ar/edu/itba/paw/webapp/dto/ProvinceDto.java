bbfc472a17ccec605321448ecb3d6aa2d099d764
lkarpovich <lkarpovich@hotmail.com>
2020-12-15 19:20:21 -0300

    added department ids to prov dto

--- webapp/src/main/java/ar/edu/itba/paw/webapp/dto/ProvinceDto.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/dto/ProvinceDto.java
@@ -2,16 +2,22 @@ package ar.edu.itba.paw.webapp.dto;
 
 import ar.edu.itba.paw.models.Province;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProvinceDto {
 
     private Long id;
     private String name;
+    private List<Long> departmentIds;
 
     public static ProvinceDto fromProvince(Province province) {
         final ProvinceDto dto = new ProvinceDto();
+        dto.departmentIds = new ArrayList<>();
 
         dto.id = province.getId();
         dto.name = province.getName();
+        province.getDepartmentList().forEach(d -> dto.departmentIds.add(d.getId()));
 
         return dto;
     }
@@ -30,5 +36,13 @@ public class ProvinceDto {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Long> getDepartmentIds() {
+        return departmentIds;
+    }
+
+    public void setDepartmentIds(List<Long> departmentIds) {
+        this.departmentIds = departmentIds;
     }
 }
