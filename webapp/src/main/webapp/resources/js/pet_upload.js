a70a3e7402af19a69374464e3feae36817225679
fastiz <facuastiz@gmail.com>
2020-12-20 18:26:56 -0300

    maven and react integration

--- webapp/src/main/webapp/resources/js/pet_upload.js
+++
@@ -1,8 +0,0 @@
-$('#adopt').on('click', function(){
-    $('#price').attr('disabled', $(this).is(':checked'));
-    if($(this).is(':checked')){
-        $('#price').val('0')
-    }else{
-        $('#price').val('')
-    }
-});
