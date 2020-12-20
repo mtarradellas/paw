f18976fcac9f73084125748fccc70ee25a9f949c
fastiz <facuastiz@gmail.com>
2020-12-19 21:13:50 -0300

    responsiveness and clear buttons clears search bar

--- webapp/src/main/reactjs/src/components/Header.js
+++ webapp/src/main/reactjs/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState, useEffect} from 'react';
+import React, {useContext, useState, useEffect, useRef} from 'react';
 import '../css/header.css';
 import {Link} from "react-router-dom";
 import {LOGIN, REGISTER, HOME, REQUESTS, INTERESTS, ADD_PET, USER} from "../constants/routes";
@@ -14,19 +14,15 @@ import {getNotifications} from "../api/requests";
 import _ from 'lodash';
 
 
-function LoggedIn() {
+function LoggedInMenuItems() {
     const {t} = useTranslation('header');
-    const {state, logout} = useLogin();
+    const {state} = useLogin();
 
     const [notifications, setNotifications] = useState({interests: null, requests: null});
 
-    const {username, id, jwt} = state;
+    const {id, jwt} = state;
 
     const {interests, requests} = notifications;
-
-    const _onLogout = () => {
-        logout();
-    };
 
     const fetchNotifications = async () => {
         try{
@@ -44,53 +40,68 @@ function LoggedIn() {
 
     return <>
 
-            <Badge className={"header__subtitle"} count={_.isNil(requests) ? 0 : requests}>
+        <div className={"header__menu-items__item"}>
+            <Badge count={_.isNil(requests) ? 0 : requests}>
                 <Link to={REQUESTS}>
                     {t('requests')}
                 </Link>
             </Badge>
+        </div>
 
-            <Badge className={"header__subtitle"} count={_.isNil(interests) ? 0 : interests}>
+        <div className={"header__menu-items__item"}>
+            <Badge count={_.isNil(interests) ? 0 : interests}>
                 <Link to={INTERESTS}>
                     {t('interests')}
                 </Link>
             </Badge>
+        </div>
 
-            <Link className={"header__subtitle"} to={USER + id}>
+        <div className={"header__menu-items__item"}>
+            <Link to={USER + id}>
                 {t('profile')}
             </Link>
+        </div>
 
-            <div className={"header__right header--session"}>
-                <SearchBar/>
-
-                <p className={"header--session--username"}>
-                    {username}
-                </p>
-
-                <Button className={"header--session--logout"} onClick={_onLogout}>
-                    {t('logout')}
-                </Button>
-
-            </div>
-        </>
+    </>
 }
 
-function NotLoggedIn(){
+function RegisterAndLogin(){
     const {t} = useTranslation('header');
 
     return <>
-        <SearchBar/>
+        <div className={"header__register-and-login"}>
+            <Link className={"header__register"} to={REGISTER}>
+                {t('register')}
+            </Link>
 
-        <div className={"header__right"}>
+            <Link className={"header__login"} to={LOGIN}>
+                {t('login')}
+            </Link>
+        </div>
+    </>
+}
 
+function UsernameAndLogout(){
+    const {t} = useTranslation('header');
+    const {state, logout} = useLogin();
 
-                <Link className={"header__register"} to={REGISTER}>
-                    {t('register')}
-                </Link>
+    const {username} = state;
 
-                <Link className={"header__login"} to={LOGIN}>
-                    {t('login')}
-                </Link>
+    const _onLogout = () => {
+        logout();
+    };
+
+    return <>
+        <div className={"header__username-and-logout"}>
+
+            <p className={"header__username-and-logout__username"}>
+                {username}
+            </p>
+
+            <Button className={"header__username-and-logout__logout"} onClick={_onLogout}>
+                {t('logout')}
+            </Button>
+
         </div>
     </>
 }
@@ -98,6 +109,7 @@ function NotLoggedIn(){
 function SearchBar() {
     const history = useHistory();
     const {t} = useTranslation('header');
+    const ref = useRef(null);
 
     const {
         onSubmitSearch,
@@ -112,7 +124,14 @@ function SearchBar() {
             history.push(HOME);
     };
 
+    useEffect(()=>{
+        if(ref.current){
+            ref.current.setFieldValue('find', find);
+        }
+    }, [find]);
+
     return <Formik
+        innerRef={ref}
         onSubmit={onSubmit}
         initialValues={
             {
@@ -127,49 +146,53 @@ function SearchBar() {
         validateOnBlur={false}
         validateOnChange={false}
         >
-                <Form layout={'inline'}>
-                    <Form.Item name={'find'}>
-                        <Input name={'find'} placeholder={t('searchPlaceholder')} allowClear/>
-                    </Form.Item>
+            <Form layout={'inline'}>
+                <Form.Item name={'find'}>
+                    <Input name={'find'} placeholder={t('searchPlaceholder')} allowClear/>
+                </Form.Item>
 
-                    <Form.Item name>
-                        <Button type="primary" htmlType="submit" loading={fetching}>
-                            {t('searchButton')}
-                        </Button>
-                    </Form.Item>
-                </Form>
+                <Form.Item name>
+                    <Button type="primary" htmlType="submit" loading={fetching}>
+                        {t('searchButton')}
+                    </Button>
+                </Form.Item>
+            </Form>
     </Formik>;
 }
 
-function Common(){
+function Header() {
     const {t} = useTranslation('header');
 
-    return <>
-        <Link className={"header__subtitle"} to={ADD_PET}>
-            {t('addPet')}
-        </Link>
-        </>
-}
-
-
-function Header() {
     const {state} = useLogin();
 
     const {isLoggedIn} = state;
 
     return <header>
-        <Link to={HOME}>
-            <img className={"header__logo"} src={"/logo.png"} alt={"logo"} width={70} height={70}/>
+        <Link to={HOME} className={"header__logo"}>
+            <img src={"/logo.png"} alt={"logo"} width={70} height={70}/>
         </Link>
 
-        <Link to={HOME}>
-            <span className={"header__title"}>
+        <Link to={HOME} className={"header__title"}>
+            <span>
                 PET SOCIETY
             </span>
         </Link>
 
-        <Common/>
-        {isLoggedIn ? <LoggedIn/> : <NotLoggedIn/>}
+        <div className={"header__menu-items"}>
+            <div className={"header__menu-items__item"}>
+                <Link to={ADD_PET}>
+                    {t('addPet')}
+                </Link>
+            </div>
+
+            <LoggedInMenuItems/>
+        </div>
+
+        <div className={"header__search-bar"}>
+            <SearchBar/>
+        </div>
+
+        {isLoggedIn ? <UsernameAndLogout/> : <RegisterAndLogin/>}
 
     </header>;
 }
