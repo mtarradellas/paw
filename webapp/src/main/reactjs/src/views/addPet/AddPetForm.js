e989ca87c4217e7ecd2e6ea5676d57262319f0ca
fastiz <facuastiz@gmail.com>
2020-12-21 11:06:59 -0300

    structuring form

--- webapp/src/main/reactjs/src/views/addPet/AddPetForm.js
+++ webapp/src/main/reactjs/src/views/addPet/AddPetForm.js
@@ -7,6 +7,7 @@ import { PlusOutlined } from '@ant-design/icons';
 import * as Yup from 'yup';
 import ConstantsContext from '../../constants/constantsContext';
 import DeleteImagesInput from "../editPet/DeleteImagesInput";
+import '../../css/addPet/addPetForm.css';
 
 const FormItem = Form.Item;
 
@@ -76,92 +77,101 @@ function AddPetForm({submitting, onSubmit, editing, initialValues}){
         render={
             ({setFieldValue, values}) => {
 
-                return <Form>
-                    <FormItem name={"petName"} label={t('form.petName.label')}>
-                        <Input name={"petName"} placeholder={t('form.petName.placeholder')}/>
-                    </FormItem>
+                return <Form className={"add-pet-form"}>
+                    
+                    <div className={"add-pet-form__group"}>
+                        <FormItem name={"petName"} label={t('form.petName.label')}>
+                            <Input name={"petName"} placeholder={t('form.petName.placeholder')}/>
+                        </FormItem>
 
-                    <FormItem name={"price"} label={t('form.price.label')}>
-                        <InputNumber
-                            name={"price"}
-                            placeholder={t('form.price.placeholder')}
-                            disabled={values.isAdoption}
-                            min={0}
-                        />
-                    </FormItem>
+                        <FormItem name={"price"} label={t('form.price.label')}>
+                            <InputNumber
+                                name={"price"}
+                                placeholder={t('form.price.placeholder')}
+                                disabled={values.isAdoption}
+                                min={0}
+                            />
+                        </FormItem>
 
-                    <FormItem name={"isAdoption"} label={t('form.isAdoption.label')}>
-                        <Checkbox name={"isAdoption"} onChange={()=>setFieldValue('price', 0)}/>
-                    </FormItem>
+                        <FormItem name={"isAdoption"} label={t('form.isAdoption.label')}>
+                            <Checkbox name={"isAdoption"} onChange={()=>setFieldValue('price', 0)}/>
+                        </FormItem>
 
-                    <FormItem name={"description"} label={t('form.description.label')}>
-                        <Input name={"description"} placeholder={t('form.description.placeholder')}/>
-                    </FormItem>
+                        <FormItem name={"description"} label={t('form.description.label')}>
+                            <Input name={"description"} placeholder={t('form.description.placeholder')}/>
+                        </FormItem>
+                    </div>
 
-                    <FormItem name={"province"} label={t('form.province.label')}>
-                        <Select name={"province"} placeholder={t('form.province.placeholder')}
-                                onChange={() => setFieldValue('department', '')}
-                        >
-                            {
-                                Object.values(provinces).map(({id, name}) => {
-                                    return <Select.Option value={id}>{name}</Select.Option>
-                                })
-                            }
-                        </Select>
-                    </FormItem>
+                    <div className={"add-pet-form__group"}>
+                        <FormItem name={"province"} label={t('form.province.label')}>
+                            <Select name={"province"} placeholder={t('form.province.placeholder')}
+                                    onChange={() => setFieldValue('department', '')}
+                            >
+                                {
+                                    Object.values(provinces).map(({id, name}) => {
+                                        return <Select.Option value={id}>{name}</Select.Option>
+                                    })
+                                }
+                            </Select>
+                        </FormItem>
 
-                    <FormItem name={"department"} label={t('form.department.label')}>
-                        <Select name={"department"} placeholder={t('form.department.placeholder')} disabled={!values.province}>
-                            {
-                                values.province && provinces[values.province].departmentIds.map(departmentId => {
-                                    const {id, name} = departments[departmentId];
+                        <FormItem name={"department"} label={t('form.department.label')}>
+                            <Select name={"department"} placeholder={t('form.department.placeholder')} disabled={!values.province}>
+                                {
+                                    values.province && provinces[values.province].departmentIds.map(departmentId => {
+                                        const {id, name} = departments[departmentId];
 
-                                    return <Select.Option value={id}>{name}</Select.Option>
-                                })
-                            }
-                        </Select>
-                    </FormItem>
+                                        return <Select.Option value={id}>{name}</Select.Option>
+                                    })
+                                }
+                            </Select>
+                        </FormItem>
 
-                    <FormItem name={"species"} label={t('form.species.label')}>
-                        <Select name={"species"} placeholder={t('form.species.placeholder')}
-                            onChange={() => setFieldValue('breed', '')}
-                        >
-                            {
-                                Object.values(species).map(({id, name}) => {
-                                    return <Select.Option value={id}>{name}</Select.Option>
-                                })
-                            }
-                        </Select>
-                    </FormItem>
+                        <FormItem name={"species"} label={t('form.species.label')}>
+                            <Select name={"species"} placeholder={t('form.species.placeholder')}
+                                    onChange={() => setFieldValue('breed', '')}
+                            >
+                                {
+                                    Object.values(species).map(({id, name}) => {
+                                        return <Select.Option value={id}>{name}</Select.Option>
+                                    })
+                                }
+                            </Select>
+                        </FormItem>
 
-                    <FormItem name={"breed"} label={t('form.breed.label')}>
-                        <Select name={"breed"} placeholder={t('form.breed.placeholder')} disabled={!values.species}>
-                            {
-                                values.species && species[values.species].breedIds.map(breedId => {
-                                    const {id, name} = breeds[breedId];
+                        <FormItem name={"breed"} label={t('form.breed.label')}>
+                            <Select name={"breed"} placeholder={t('form.breed.placeholder')} disabled={!values.species}>
+                                {
+                                    values.species && species[values.species].breedIds.map(breedId => {
+                                        const {id, name} = breeds[breedId];
 
-                                    return <Select.Option value={id}>{name}</Select.Option>
-                                })
-                            }
-                            <Select.Option value={"breed"}>breed</Select.Option>
-                        </Select>
-                    </FormItem>
+                                        return <Select.Option value={id}>{name}</Select.Option>
+                                    })
+                                }
+                                <Select.Option value={"breed"}>breed</Select.Option>
+                            </Select>
+                        </FormItem>
+                    </div>
 
-                    <FormItem name={"dateOfBirth"} label={t('form.dateOfBirth.label')}>
-                        {/*TODO: localizar*/}
-                        <DatePicker name={"dateOfBirth"}/>
-                    </FormItem>
+                    <div className={"add-pet-form__group"}>
 
-                    <FormItem name={"isVaccinated"} label={t('form.isVaccinated.label')}>
-                        <Checkbox name={"isVaccinated"}/>
-                    </FormItem>
+                        <FormItem name={"dateOfBirth"} label={t('form.dateOfBirth.label')}>
+                            {/*TODO: localizar*/}
+                            <DatePicker name={"dateOfBirth"}/>
+                        </FormItem>
 
-                    <FormItem name={"gender"} label={t('form.gender.label')}>
-                        <Select name={"gender"} placeholder={t('form.gender.placeholder')}>
-                            <Select.Option value={'male'}>{t('form.gender.male')}</Select.Option>
-                            <Select.Option value={'female'}>{t('form.gender.female')}</Select.Option>
-                        </Select>
-                    </FormItem>
+                        <FormItem name={"isVaccinated"} label={t('form.isVaccinated.label')}>
+                            <Checkbox name={"isVaccinated"}/>
+                        </FormItem>
+
+                        <FormItem name={"gender"} label={t('form.gender.label')}>
+                            <Select name={"gender"} placeholder={t('form.gender.placeholder')}>
+                                <Select.Option value={'male'}>{t('form.gender.male')}</Select.Option>
+                                <Select.Option value={'female'}>{t('form.gender.female')}</Select.Option>
+                            </Select>
+                        </FormItem>
+                    </div>
+                    
 
                     {
                         editing && <DeleteImagesInput setFieldValue={setFieldValue} values={values}/>
