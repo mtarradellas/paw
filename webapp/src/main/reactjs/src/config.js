e8d56c397d9adea43f35ab34900faf3b14487de9
manuel tarradellas <mtarradellas@itba.edu.ar>
2021-01-30 16:31:10 -0300

    react dedploy config

--- webapp/src/main/reactjs/src/config.js
+++ webapp/src/main/reactjs/src/config.js
@@ -1,5 +1,5 @@
 
 // export const CONTEXT = '';
 // export const SERVER_URL = CONTEXT + '/api';
-export const SERVER_URL = 'http://localhost:8080/paw-2020a-7/api';
+export const SERVER_URL = 'http://pawserver.it.itba.edu.ar/paw-2020a-7/api';
 export const CONTEXT = '/paw-2020a-7';
